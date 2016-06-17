885f3bdec6c8eacf7e827342a3fe20e920e9ad76
Maciej Kurek <maciejk77@gmail.com>
2016-06-17 17:20:35 +0100

    Tasks completed, refactoring done v1

--- task1.js
+++ task1.js
@@ -1,20 +1,16 @@
 // Make this work (repeat 3 times the contents of an array):
 // repeat([1,2,3]) //[1,2,3,1,2,3,1,2,3]
 
-// Calling a function with a string stored in text variable (arr)
-repeat(arr);
-
-var arr = [1,2,3]; 
 var newArr = [];
 
+// Function repeats array elements as many times as a length of the array
+repeat([1,2,3]);
+
 function repeat(arr) {
-  for(var numOfArrs=0; numOfArrs<arr.length; numOfArrs++) {
-    for(var numOfEl=0; numOfEl<arr.length; numOfEl++) {
-      newArr.push(arr[numOfEl]);
+  for(var j=0; j<arr.length; j++) {
+    for(var j=0; j<arr.length; j++) {
+      newArr.push(arr[j]);
     }
   }
   return newArr;
 }
-
-
-
