7bad536bfe320b9c8e7e55ac1b3fbd370ee14d9f
Maciej Kurek <maciejk77@gmail.com>
2016-06-17 22:01:00 +0100

    Tasks completed, refactoring done v2

--- task1.js
+++ task1.js
@@ -1,15 +1,15 @@
 // Make this work (repeat 3 times the contents of an array):
-// repeat([1,2,3]) //[1,2,3,1,2,3,1,2,3]
+// repeat([1,2,3]) => [1,2,3,1,2,3,1,2,3]
 
 var newArr = [];
 
 // Function repeats array elements as many times as a length of the array
 repeat([1,2,3]);
 
-function repeat(arr) {
-  for(var j=0; j<arr.length; j++) {
-    for(var j=0; j<arr.length; j++) {
-      newArr.push(arr[j]);
+function repeat(array) {
+  for(var i=0; i<array.length; i++) {
+    for(var j=0; j<array.length; j++) {
+      newArr.push(array[j]);
     }
   }
   return newArr;
--- task3.js
+++ task3.js
@@ -2,18 +2,18 @@
 //return the next binary number in a string or as an array)
 // next_binary_number([1,0]) ==> [1,1]
 
-var decArr = []; 
-var num = 0; 
+var decimalArray = []; 
+var number = 0; 
 
 // Calling a function with an array of binary numbers ( [1,0] )
 next_binary_number([1,0]);
 
 // Function to convert binary to decimal, increment and back to binary
-function next_binary_number(binArr) {
-  var x = bin2dec(binArr);
-  x++;
-  var y = dec2bin(x);
-  return y;
+function next_binary_number(binArray) {
+  var decimal = bin2dec(binArray);
+  decimal++;
+  var binary = dec2bin(decimal);
+  return binary;
 }
 
 // Function to convert binary to decimal number
@@ -23,26 +23,26 @@ function bin2dec(binArray) {
   // loop to calculate num value
   for(var i=0; i<revBinArray.length; i++) { 
     if(revBinArray[i] === 1 ) { 
-      num = num + Math.pow(2,i) 
+      number = number + Math.pow(2,i) 
     } 
   }
-  return num;
+  return number;
 }
 
 // Function to convert decimal to binary number
-function dec2bin(decNum) {
-  while(decNum >= 2) {  
+function dec2bin(decimalNumber) {
+  while(decimalNumber >= 2) {  
     // Getting a remainder of the binary division
-    remainder = decNum % 2;
+    remainder = decimalNumber % 2;
     // Getting a decimal number
-    decNum = (decNum - remainder) / 2;
+    decimalNumber = (decimalNumber - remainder) / 2;
     // Pushing reminder to the array of 0's and 1's
-    decArr.push(remainder);
+    decimalArray.push(remainder);
   }
   // Adding a last quotient to the array
-  decArr.push(decNum);
+  decimalArray.push(decimalNumber);
   // Reversing the order to get the final binary number array
-  return decArr.reverse();
+  return decimalArray.reverse();
 }
 
 
